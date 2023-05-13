d61e148d3525abf9ee67cd6a11feff5c65055d29
pgk95 <kalupgk@gmail.com>
2023-05-13 03:39:33 -0700

    quick fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
 
+"""
+models/base_model.py:
+Defines the BaseModel class, serving as the base class for other
+classes.
+"""
+
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
+    """
+    defines common attributes and methods for the other classes.
+    """
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def save(self):
+        """
+        Updates the updated_at attribute with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing al keys/values of the instance.
+        """
         data = self.__dict__.copy()
         data['__class__'] = self.__class__.__name__
         data['created_at'] = self.created_at.isoformat()
@@ -17,4 +36,7 @@ class BaseModel:
         return data
 
     def __str__(self):
+        """
+        Returns a string representation of the instance.
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
