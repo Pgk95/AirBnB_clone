4b85f75f6aa352570fd2cad3eb1eaa768708e9f7
pgk95 <kalupgk@gmail.com>
2023-05-14 06:07:51 -0700

    quick fix

--- models/base_model.py
+++ models/base_model.py
@@ -11,10 +11,17 @@ class BaseModel:
     for classes
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **Kwargs):
+        if Kwargs:
+            for key, val in Kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
